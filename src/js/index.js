19e580ef2f59472ff46d824bbe7b83f76602d771
Denis <den.neposeda@gmail.com>
2018-06-04 01:47:42 +0400

    Готовый проект, без Docker

---
+++ src/js/index.js
@@ -0,0 +1,23 @@
+/* import _ from 'lodash'; */
+/* import printMe from './print.js'; */
+
+import '../sass/layout/base.scss'
+import '../sass/style.scss';
+import '../sass/section/footer.scss';
+
+/* function component() {
+  var element = document.createElement('div');
+  var btn = document.createElement('button');
+
+  element.innerHTML = _.join(['Hello', 'webpack', '!', 'lodash'], ' ');
+  
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
+  
+  element.appendChild(btn);
+  
+  
+  return element;
+}
+
+document.body.appendChild(component()); */
---
+++ webpack.config.js
@@ -0,0 +1,68 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+	
+  entry: {
+    app: './src/js/index.js',
+  },
+  
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  
+  devtool: 'source-map',
+ // devServer: {
+ //   contentBase: './dist'
+ // },
+ 
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+		  {
+			loader: "css-loader",
+			options: { minimize: true }
+          },
+		  {
+			loader: 'postcss-loader',
+		  },
+		  {
+			loader: "sass-loader",
+          }
+        ]
+      },
+    ]
+  },
+  
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+	
+    new HtmlWebpackPlugin({
+/* 	  minify: {
+        collapseWhitespace: true
+      }, */
+	  template: 'src/html/index.html'
+    }),
+	
+	new MiniCssExtractPlugin({
+      filename: "style.css",
+    }),
+	
+	new CopyWebpackPlugin([
+		{
+			from: './src/img/',
+			to: 'img/[name].[ext]',
+		}
+	]),
+  ],
+
+  
+
+};
